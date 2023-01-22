e95fca97232742a19e35f35c71f0fffeed0b2fc5
pineappdev <Jecet4@wp.pl>
2023-01-22 13:23:55 +0100

    Added A* implementation

--- localization_agent_task.py
+++ localization_agent_task.py
@@ -1,5 +1,5 @@
 from queue import PriorityQueue
-from typing import Dict, Tuple
+from typing import Dict, Tuple, Generator
 
 import cv2
 import numpy as np
@@ -8,7 +8,31 @@ from environment import Environment
 from utils import generate_maze
 
 
-def a_star_search(occ_map: np.ndarray, start: Tuple[int, int], end: Tuple[int, int]) -> Dict[Tuple[int, int], Tuple[int, int]]:
+def manhattan_dist(vec1: Tuple[int, int], vec2: Tuple[int, int]) -> int:
+    """
+    Compute distance between two points represented as 2d tuples.
+    """
+    return sum(abs(x - y) for x, y in zip(vec1, vec2))
+
+
+def is_valid_pos_on_occ_map(occ_map: np.ndarray, pos: np.ndarray) -> bool:
+    for dim in range(occ_map.ndim):
+        if pos[dim] < 0 or pos[dim] >= occ_map.shape[dim]:
+            return False
+
+    return True
+
+
+def empty_neighbours(occ_map: np.ndarray, cell: Tuple[int, int]) -> Generator[Tuple[int, int], None, None]:
+    for neighbor_pos in [np.sum(list(zip(cell, pos_alteration)), axis=1) for pos_alteration in
+                         [(0, 1), (1, 0), (-1, 0), (0, -1)]]:
+        if is_valid_pos_on_occ_map(occ_map, neighbor_pos) and not (occ_map[tuple(neighbor_pos)]):
+            yield tuple(x for x in neighbor_pos)
+
+
+# TODO: write tests for that
+def a_star_search(occ_map: np.ndarray, start: Tuple[int, int], end: Tuple[int, int]) -> Dict[
+    Tuple[int, int], Tuple[int, int]]:
     """
     Implements the A* search with heuristic function being distance from the goal position.
     :param occ_map: Occupancy map, 1 – field is occupied, 0 – is not occupied.
@@ -17,7 +41,34 @@ def a_star_search(occ_map: np.ndarray, start: Tuple[int, int], end: Tuple[int, i
     :return: The dictionary containing at least the optimal path from start to end in the form:
         {start: intermediate, intermediate: ..., almost: goal}
     """
-    """ TODO: your code goes here """
+    Node = Tuple[int, int]
+
+    fringe = PriorityQueue()
+    prev_node_on_best_path: Dict[Node, Node] = dict()
+    lowest_cost_for_node: Dict[Node, int] = {start: 0}
+    fringe.put((manhattan_dist(start, end), (start, 0)))  # f, (node, cost to get to it)
+
+    while (cur_node := fringe.get())[1][0] != end:
+        cur_node_h, (cur_node_pos, dist_to_cur_node_from_start) = cur_node
+
+        for neighbour_pos in empty_neighbours(occ_map, cur_node_pos):
+            cost_for_neighbour = dist_to_cur_node_from_start + 1
+            if lowest_cost_for_node.get(neighbour_pos, occ_map.size ** 2) > cost_for_neighbour:
+                fringe.put((manhattan_dist(neighbour_pos, end) + cost_for_neighbour,
+                            (neighbour_pos,
+                             cost_for_neighbour)))
+                lowest_cost_for_node[neighbour_pos] = cost_for_neighbour
+                prev_node_on_best_path[neighbour_pos] = cur_node_pos
+
+    ans: Dict[Node, Node] = dict()
+    next_node = end
+
+    while next_node != start:
+        prev_node = prev_node_on_best_path.get(next_node)
+        ans[prev_node] = next_node
+        next_node = prev_node
+
+    return ans
 
 
 class LocalizationMap:
